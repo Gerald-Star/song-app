32b747b07de8368ea47e730f146df7ac55c4c770
Gerald-Star <chimagerald3@gmail.com>
2022-12-05 01:54:19 +0100

    coomit: added ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,39 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import {useState} from 'react'
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  console.log('expeireence')
+
+   // const clickHandler = () =>{
+  //   console.log('Clicked!!') 
+  // create a function clickhandler keyword and pass it to button
+  //this is how to add even listener to our state
+  // };
+
+  const clickHandler = () => {
+    setTitle ('Updated'); //calling the useState updating function
+    console.log(title);
+  };
+
+
+
   return (
     <div className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler }>Change Title</button>
     </div>
   );
 }
 
 export default ExpenseItem;
+
+//Every event needs a value that assigns to what this event should do
+//the value on the onClick event is clickHandler. 
+//To define this value, we to declare a function (const) that holds
+//a variable. This makes your code lean by not defining the function inside the onClick.
